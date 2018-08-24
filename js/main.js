c64213ec0e5e8d59e8abcbf270981f73985fb095
Dorcolac <nikola.starcevic@hotmail.rs>
2018-08-24 09:01:09 +0200

    updated structure, content, design

--- js/main.js
+++ js/main.js
@@ -3,30 +3,53 @@ $('.manufacturers').slick({
 	accessibility: true,
 	autoplay: true,
 	autoplaySpeed: 1500,
-	slidesToShow: 1,
-	arrows: false,
-	centerMode: true,
-	slidesToScroll: 1,
-	mobileFirst: true,
-	centerMode: true,
-	centerPadding: "0",
+	speed: 200,
+	slidesToShow: 3,
 	arrows: true,
+	centerMode: true,
+	slidesToScroll: 2,
+	mobileFirst: true,
+	centerPadding: "0",
+	infinite: true,
 	responsive: [
-	{
-		breakpoint: 900,
-		settings: {
-			slidesToShow: 5
-
-		}
-	},
 	{
 		breakpoint: 450,
 		settings: {
-			slidesToShow: 3
-
+			slidesToShow: 3,
+			arrows: true
+		}
+	},
+	{
+		breakpoint: 900,
+		settings: {
+			slidesToShow: 5,
+			arrows: true
 		}
 	}
 	]
+});
+
+$('.manufacturers').on('click', '.slick-slide', function (e) {
+	e.stopPropagation();
+	$([document.documentElement, document.body]).animate({
+        scrollTop: $("#proizvodjaci").offset().top
+    }, 200);
+	var index = $(this).data("slick-index");
+	if ($('.manufacturers').slick('slickCurrentSlide') !== index) {
+		var name = $(this).find('img').attr('title');
+		var desc = $(this).find('img').attr('data-desc');
+		$('.manufacturers').slick('slickGoTo', index);
+		$('.man-info h3').html(name);
+		$('.man-info p').html(desc);
+		$('.man-info').slideDown();
+		console.log($(this).find("img").attr('src'));
+	} else {
+		var name = $(this).find('img').attr('title');
+		var desc = $(this).find('img').attr('data-desc');
+		$('.man-info h3').html(name);
+		$('.man-info p').html(desc);
+		$('.man-info').slideToggle();
+	}
 });
 
 $('.parallax-window').parallax({imageSrc: 'img/engine.jpg'});
