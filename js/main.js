aa26c29e31a773d6eaaa54576cbbe6bbff939cb6
Dorcolac <nikola.starcevic@hotmail.rs>
2018-08-30 20:03:26 +0200

    save before system reinstall

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $('.manufacturers').slick({
 	autoplay: true,
 	autoplaySpeed: 1500,
 	speed: 200,
-	slidesToShow: 3,
+	slidesToShow: 1,
 	arrows: true,
 	centerMode: true,
 	slidesToScroll: 2,
@@ -32,24 +32,40 @@ $('.manufacturers').slick({
 $('.manufacturers').on('click', '.slick-slide', function (e) {
 	e.stopPropagation();
 	$([document.documentElement, document.body]).animate({
-        scrollTop: $("#proizvodjaci").offset().top
-    }, 200);
+		scrollTop: $(".manufacturers").offset().top
+	}, 200);
 	var index = $(this).data("slick-index");
-	if ($('.manufacturers').slick('slickCurrentSlide') !== index) {
+	// if ($('.manufacturers').slick('slickCurrentSlide') !== index) {
 		var name = $(this).find('img').attr('title');
+		var src = $(this).find('img').attr('src');
 		var desc = $(this).find('img').attr('data-desc');
+		var web = $(this).find('img').attr('data-web');
+		var cat = $(this).find('img').attr('data-cat');
 		$('.manufacturers').slick('slickGoTo', index);
 		$('.man-info h3').html(name);
 		$('.man-info p').html(desc);
+		$('.man-info .side .catalogue a').attr('href', cat);
+		$('.man-info .side .web a').attr('href', web);
+		$('.man-info .side img').attr('src', src);
 		$('.man-info').slideDown();
-		console.log($(this).find("img").attr('src'));
-	} else {
-		var name = $(this).find('img').attr('title');
-		var desc = $(this).find('img').attr('data-desc');
-		$('.man-info h3').html(name);
-		$('.man-info p').html(desc);
-		$('.man-info').slideToggle();
-	}
+		$('.manufacturers').slick('slickPause');
+	// } else {
+	// 	var name = $(this).find('img').attr('title');
+	// 	var desc = $(this).find('img').attr('data-desc');
+	// 	var web = $(this).find('img').attr('data-web');
+	// 	var cat = $(this).find('img').attr('data-cat');
+	// 	$('.man-info .main h3').html(name);
+	// 	$('.man-info .main p').html(desc);
+	// 	$('.man-info .side .catalogue a').attr('href', cat);
+	// 	$('.man-info .side .web a').attr('href', web);
+	// }
 });
 
-$('.parallax-window').parallax({imageSrc: 'img/engine.jpg'});
+$('.close-info').on('click', function() {
+	$('.man-info').slideUp();
+	$('.manufacturers').slick('slickPlay');
+})
+
+$('.burger').on('click', function() {
+	$('nav').slideToggle();
+})
