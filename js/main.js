b6a25e2d9dac1e3a2b3b7c8e3c085c8ba6369334
Dorcolac <nikola.starcevic@hotmail.rs>
2018-09-07 23:35:07 +0200

    update

--- js/main.js
+++ js/main.js
@@ -35,30 +35,19 @@ $('.manufacturers').on('click', '.slick-slide', function (e) {
 		scrollTop: $(".manufacturers").offset().top
 	}, 200);
 	var index = $(this).data("slick-index");
-	// if ($('.manufacturers').slick('slickCurrentSlide') !== index) {
-		var name = $(this).find('img').attr('title');
-		var src = $(this).find('img').attr('src');
-		var desc = $(this).find('img').attr('data-desc');
-		var web = $(this).find('img').attr('data-web');
-		var cat = $(this).find('img').attr('data-cat');
-		$('.manufacturers').slick('slickGoTo', index);
-		$('.man-info h3').html(name);
-		$('.man-info p').html(desc);
-		$('.man-info .side .catalogue a').attr('href', cat);
-		$('.man-info .side .web a').attr('href', web);
-		$('.man-info .side img').attr('src', src);
-		$('.man-info').slideDown();
-		$('.manufacturers').slick('slickPause');
-	// } else {
-	// 	var name = $(this).find('img').attr('title');
-	// 	var desc = $(this).find('img').attr('data-desc');
-	// 	var web = $(this).find('img').attr('data-web');
-	// 	var cat = $(this).find('img').attr('data-cat');
-	// 	$('.man-info .main h3').html(name);
-	// 	$('.man-info .main p').html(desc);
-	// 	$('.man-info .side .catalogue a').attr('href', cat);
-	// 	$('.man-info .side .web a').attr('href', web);
-	// }
+	var name = $(this).find('img').attr('title');
+	var src = $(this).find('img').attr('src');
+	var desc = $(this).find('img').attr('data-desc');
+	var web = $(this).find('img').attr('data-web');
+	var cat = $(this).find('img').attr('data-cat');
+	$('.manufacturers').slick('slickGoTo', index);
+	$('.man-info h3').html(name);
+	$('.man-info p').html(desc);
+	$('.man-info .side .catalogue a').attr('href', cat);
+	$('.man-info .side .web a').attr('href', web);
+	$('.man-info .side img').attr('src', src);
+	$('.man-info').slideDown();
+	$('.manufacturers').slick('slickPause');
 });
 
 $('.close-info').on('click', function() {
@@ -66,6 +55,7 @@ $('.close-info').on('click', function() {
 	$('.manufacturers').slick('slickPlay');
 })
 
-$('.burger').on('click', function() {
+$('.navTrigger').click(function(){
 	$('nav').slideToggle();
-})
+	$(this).toggleClass('active');
+});
